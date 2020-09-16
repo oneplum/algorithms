8cf0369378c176ee9cf9c433cf770713825f6246
oneplum <leesasha90@gmail.com>
2020-09-16 09:58:27 +0800

    modify algorithm

--- sort/quick-sort/LinkListQuickSort.php
+++ sort/quick-sort/LinkListQuickSort.php
@@ -10,55 +10,63 @@ class ListNode {
     }
 }
 
-function QuickSort($list, $head, $tail = null) {
-    if (isLowerP($head, $tail)) {
-        $m = partition($list, $head, $tail);
-        QuickSort($list, $head, $m);
-        QuickSort($list, $m->next, $tail);
-    }
-}
+class SingleLinedList {
+    protected $head;
 
-function partition($list, $head, $tail = null) {
-    $x = $head->val;
-    $i = $head;
-    $j = $head->next;
-    while ($j !== $tail) {
-        if ($j->val <= $x) {
-            $i =  $i->next;
-            $tmp = $i->val;
-            $i->val = $j->val;
-            $j->val = $tmp;
+    public function addNode($val) {
+        if (is_null($this->head)) {
+            $this->head = new ListNode($val);
+        } else {
+            $t = $this->head;
+            while (!is_null($t->next)) {
+                $t = $t->next;
+            }
+            $t->next = new ListNode($val);
         }
-        $j = $j->next;
     }
-    $tmp = $head->val;
-    $head->val = $i->val;
-    $i->val = $tmp;
-    return $i;
-}
-
-function isLowerP($head, $tail) {
-    if ($head === $tail) {
-        return false;
-    }
-    if (is_null($tail) && !is_null($head)) {
-        return true;
-    }
-    $tmp = $head;
-    while (!is_null($tmp)) {
-        if ($tmp === $tail) {
-            return true;
+    public function display() {
+        $head = $this->head;
+        while (!is_null($head)) {
+            echo $head->val.' ';
+            $head = $head->next;
         }
-        $tmp = $tmp->next;
+        echo "\r\n";
     }
-    return false;
+    public function sort() {
+        $this->_sort($this->head);
+    }
+    private function _sort($head, $tail = null) {
+        if ($head !== $tail) {
+            $m = $this->_partition($head, $tail);
+            $this->_sort($head, $m);
+            $this->_sort($m->next, $tail);
+        }
+    }
+    private function _partition($head, $tail = null) {
+        $x = $head->val;
+        $i = $head;
+        $j = $head->next;
+        while ($j !== $tail) {
+            if ($j->val <= $x) {
+                $i =  $i->next;
+                $tmp = $i->val;
+                $i->val = $j->val;
+                $j->val = $tmp;
+            }
+            $j = $j->next;
+        }
+        $tmp = $head->val;
+        $head->val = $i->val;
+        $i->val = $tmp;
+        return $i;
+    }
 }
 
-function display($list) {
-    $head = $list;
-    while (!is_null($head)) {
-        echo $head->val.' ';
-        $head = $head->next;
-    }
-    echo "\r\n";
+$list = new SingleLinedList();
+$n = mt_rand(2, 100);
+for ($i=0;$i<$n;++$i) {
+    $list->addNode(mt_rand(-100, 10000));
 }
+$list->display();
+$list->sort();
+$list->display();
